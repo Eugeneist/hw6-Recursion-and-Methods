65f70d336f9fcf0d1df3bd5b8440bc193fb47631
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-02-07 16:18:34 +0200

    added short version of task #1

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ const sumTo = (n) => {
 console.log(sumTo(5));
 console.log(sumTo(4));
 console.log(sumTo(3));
+
+// короткая версия:
+
+const sumToShort = (n) => (n === 1) ? n : n + sumToShort(n - 1);
+
+console.log(sumToShort(4));
+console.log(sumToShort(3));
+
+
+// ЗАДАЧА 2
+
+
